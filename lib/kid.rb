4336f903d82c38a86eb9fe4df77e3c5af24464ff
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 18:58:58 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,5 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
+  extend MetaDancing
 end
