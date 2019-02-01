a7948c1b8bbd30477622d8fff55e7fe69371fd0e
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-01 19:04:13 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,8 +1,15 @@
+5 lines (11 sloc)  276 Bytes
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
   extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+  include FancyDance::InstanceMethods 
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
